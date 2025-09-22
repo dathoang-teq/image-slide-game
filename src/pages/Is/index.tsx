7ed891216f29171b9fcbb57eada1752bfd3266ff
dathoang-teq <dathoang@teqnological.asia>
2025-09-22 15:24:26 +0700

    chore: change starf to stabler

--- src/pages/Is/index.tsx
+++ src/pages/Is/index.tsx
@@ -7,8 +7,8 @@ import image3 from "../../assets/images/three/calendar.webp";
 import image4 from "../../assets/images/three/KEYBOARD.jpg";
 import image5 from "../../assets/images/three/lipstick.avif";
 import image6 from "../../assets/images/three/Rice_Cooker.webp";
-import image7 from "../../assets/images/three/Scalf.jpg";
 import image8 from "../../assets/images/three/SMART-PHONE.jpg";
+import image7 from "../../assets/images/three/stapler.webp";
 import image9 from "../../assets/images/three/towel.jpg";
 import image10 from "../../assets/images/three/TRUCK.webp";
 
